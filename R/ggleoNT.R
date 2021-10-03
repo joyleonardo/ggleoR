e44c84934b7492882d34b2700fca249ee69e1cbc
joyleonardo <loujiayi318@gmail.com>
2021-10-03 19:41:53 -0400

    update
    update

--- R/ggleoNT.R
+++ R/ggleoNT.R
@@ -1,7 +1,7 @@
 #' Title
 #'
 #' @param f  the function with any variables
-#' @param x  the gussing start point
+#' @param x  the guessing start point
 #' @param tol Accuracy ex(0.001)
 #' @param N The maximum number of iterations
 #'
@@ -9,17 +9,20 @@
 #' @export
 #'
 #' @examples
+#'library(ggleo)
+#' ggleoNT(f,x,tol,N)
+
+
 ggleoNT=function(f,x,tol,N){
+
   diff<-4
   iter<-0
   while ((diff>tol) && (iter<N)) {
     oldx<-x
-    x<-x-solve(numDeriv::hessian(ly, x))%*%numDeriv::grad(ly, x)
+    x<-x-solve(hessian(ly, x))%*%grad(ly, x)
     diff=sqrt(sum((x-oldx)^2))
     iter<-iter+1
     cat('this is ',iter,'th iteration,', 'x is ',x,'the value is',ly(x), ',difference norm is',diff,'\n')
   }
   return(x)
 }
-
-devtools::document()
